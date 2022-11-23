fe4f0b30a154e8af9df29f64eb310f3f42b53f9a
Alysson Marley Dias <callistomain@gmail.com>
2022-11-23 10:43:13 -0300

    refact: signup

--- src/pages/Signup/SignupPage.js
+++ src/pages/Signup/SignupPage.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../../components/Button";
+import { InputText } from "../../components/InputText";
 import { url } from "../../constants/urls";
 import { SignupStyle } from "./SignupStyle";
 
@@ -29,11 +31,11 @@ export default function SignupPage() {
     <SignupStyle>
       <h1>GameStore</h1>
       <form action="" onSubmit={submitHandler}>
-        <input type="text" name="name" id="name" placeholder="Nome" required/>
-        <input type="email" name="email" id="email" placeholder="E-mail" required/>
-        <input type="password" name="password" id="password" placeholder="Senha" required/>
-        <input type="password" name="pwconfirm" id="pwconfirm" placeholder="Confirme a senha" required/>
-        <button>Cadastrar</button>
+        <InputText type="text" name="name" id="name" placeholder="Nome" required/>
+        <InputText type="email" name="email" id="email" placeholder="E-mail" required/>
+        <InputText type="password" name="password" id="password" placeholder="Senha" required/>
+        <InputText type="password" name="pwconfirm" id="pwconfirm" placeholder="Confirme a senha" required/>
+        <Button>Cadastrar</Button>
       </form>
       <Link to="/login">Já tem uma conta? Entre agora!</Link>
     </SignupStyle>
--- src/pages/Signup/SignupStyle.js
+++ src/pages/Signup/SignupStyle.js
@@ -24,45 +24,7 @@ export const SignupStyle = styled.div`
     flex-direction: column;
     gap: 8px;
     margin-bottom: 16px;
-  }
-
-  input {
-    display: flex;
-    align-items: center;
-    border: 1px solid #17313a;
-    background: #102531;
-    padding: 5px 10px;
-
-    height: 40px;
-    width: 300px;
-    font-size: 16px;
-    outline: none;
-    color: white;
-  }
-
-  input:-webkit-autofill {
-      -webkit-box-shadow: 0 0 0 30px #102531 inset !important;
-      -webkit-text-fill-color: white !important;
-      caret-color: white;
-  }
-  
-  input:focus,
-  input:focus-visible {
-    border: 1px solid #d47559;
-  }
-
-  button {
-    cursor: pointer;
-    text-align: center;
-    border-radius: 3px;
-    background: #123040;
-    padding: 10px;
-    color: white;
-
-    height: 40px;
-    width: 300px;
-    font-weight: bold;
-    font-size: 16px;
+    width: 100%;
   }
 
   a {
