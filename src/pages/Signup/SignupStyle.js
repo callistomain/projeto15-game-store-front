3d7e7cd756dd47376db8bb21f374903450f6a464
Alysson Marley Dias <callistomain@gmail.com>
2022-11-23 11:41:07 -0300

    refact: SignupStyle

--- src/pages/Signup/SignupStyle.js
+++ src/pages/Signup/SignupStyle.js
@@ -1,33 +1,6 @@
 import styled from 'styled-components';
-import { colorBG } from '../../constants/colors';
+import { LoginStyle } from '../Login/LoginStyle';
 
-export const SignupStyle = styled.div`
-  height: 100vh;
-  width: 100vw;
-  background-color: ${colorBG};
-  color: white;
+export const SignupStyle = styled(LoginStyle)`
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-
-  h1 {
-    font-size: 32px;
-    margin-bottom: 16px;
-  }
-
-  form {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 8px;
-    margin-bottom: 16px;
-    width: 100%;
-  }
-
-  a {
-    color: white;
-  }
 `;
