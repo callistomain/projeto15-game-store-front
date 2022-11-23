cb8afc26e1a09f34b939b61d5f5052d5acc31c86
Alysson Marley Dias <callistomain@gmail.com>
2022-11-23 08:44:03 -0300

    feat: empty signup page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import GlobalStyled from "./GlobalStyled";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import SignupPage from "./pages/Signup/SignupPage";
 
 export default function App() {
   return (
     <Router>
       <GlobalStyled/>
       <Routes>
-        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/signup" element={<SignupPage/>}/>
       </Routes>
     </Router>
   );
---
+++ src/pages/Signup/SignupPage.js
@@ -0,0 +1,9 @@
+import { SignupStyle } from "./SignupStyle";
+
+export default function SignupPage() {
+  return (
+    <SignupStyle>
+      Hello
+    </SignupStyle>
+  );
+}
