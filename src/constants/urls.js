49f4b88fca154c1f7581ffaeab295fd19acbb7a9
Alysson Marley Dias <callistomain@gmail.com>
2022-11-27 21:53:28 -0300

    Update urls.js

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/"; // https://game-store-api.onrender.com/
+const BASE_URL = "https://game-store-api.onrender.com/";
 
 export const url = {
   signup: BASE_URL + "signup",
